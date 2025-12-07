3cfc5150a4a02660e7f952d9ef5227b02416d272
Dyzaster <46618211+Dy-zaster@users.noreply.github.com>
2025-12-07 20:14:50 -0300

    Revert "avances servidor"
    This reverts commit e1302e0d3db9f9c50a6ed6288902cc6c584db249.

--- Servidor/src/ArtesArcanas.Server.Core/Networking/ILegacyCommandSink.cs
+++
@@ -1,6 +0,0 @@
-namespace ArtesArcanas.Server.Core.Networking;
-
-public interface ILegacyCommandSink
-{
-    Task EnqueueAsync(ushort sessionCode, ReadOnlyMemory<byte> data, CancellationToken cancellationToken);
-}
--- Servidor/src/ArtesArcanas.Server.Core/World/LegacyInventorySlot.cs
+++
@@ -1,8 +0,0 @@
-namespace ArtesArcanas.Server.Core.World;
-
-internal readonly record struct LegacyInventorySlot(byte ItemId, byte Modifier)
-{
-    public bool IsEmpty => ItemId == 0 && Modifier == 0;
-
-    public static LegacyInventorySlot Empty => new(0, 0);
-}
